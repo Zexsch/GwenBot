ed9d2fe958072af95e4b0ee11c13ee34c85ef4b0
Zexsch <103447968+Zexsch@users.noreply.github.com>
2023-09-27 14:22:38 +0200

    Update database.py

--- Database/database.py
+++ Database/database.py
@@ -9,6 +9,7 @@ class Database():
             try:
                 cur.execute('CREATE TABLE Subs(id INTEGER PRIMARY KEY, user_id INTEGER, server_id INTEGER)')
                 cur.execute('CREATE TABLE Blacklist(id INTEGER PRIMARY KEY, user_id INTEGER, server_id INTEGER)')
+                cur.execute('CREATE TABLE Quote(id INTEGER PRIMARY KEY, server_id INTEGER)')
             except sqlite3.OperationalError:
                 pass
 
@@ -27,6 +28,15 @@ class Database():
         with sqlite3.connect('GwenUsers') as con:
             cur = con.cursor()
             res = cur.execute('SELECT * FROM Blacklist WHERE user_id=? AND server_id=?',(user_id, server_id)).fetchall()
+            
+            return True if res else False
+        
+    def fetch_quote(self, server_id: int) -> bool:
+        """Return True if server uses quote, else return False"""
+        
+        with sqlite3.connect('GwenUsers') as con:
+            cur = con.cursor()
+            res = cur.execute('SELECT * FROM Blacklist WHERE server_id=?',(server_id,)).fetchall()
             
             return True if res else False
         
@@ -57,7 +67,14 @@ class Database():
         with sqlite3.connect('GwenUsers') as con:
             cur = con.cursor()
             cur.execute('INSERT INTO Blacklist(user_id, server_id) VALUES(?,?)', (user_id, server_id))
-    
+            
+    def add_to_quote(self, server_id: int) -> None:
+        """Add server to quote"""
+        
+        with sqlite3.connect('GwenUsers') as con:
+            cur = con.cursor()
+            cur.execute('INSERT INTO Quote(server_id) VALUES(?)', (server_id,))
+     
     def remove_from_gwen_sub(self, user_id: int, server_id: int) -> bool:
         """Remove user from GwenBot subscription. Return true if successfull else false."""
         
@@ -78,3 +95,10 @@ class Database():
             cur = con.cursor()
             
             cur.execute('DELETE FROM Blacklist WHERE user_id=? AND server_id=?', (user_id, server_id))
+            
+    def remove_from_quote(self, server_id: int) -> None:
+        """Remove server from quote"""
+        
+        with sqlite3.connect('GwenUsers') as con:
+            cur = con.cursor()
+            cur.execute('DELETE FROM Quote WHERE server_id=?', (server_id,))
