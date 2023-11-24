da7c0f85b828252db1192fefffe4d4e3138eb1a0
Zexsch <103447968+Zexsch@users.noreply.github.com>
2023-11-24 20:13:50 +0100

    Update config.py

--- Config/config.py
+++ Config/config.py
@@ -1,5 +1,6 @@
 LOL_VERSION: str = '12.22.1'  # Change the version to whatever the newest version is. Format: Season.Patch.1
 OWNER_ID: int = 123  # Change to your own discord user ID
 DEFAULT_CHANNEL: int = 123  # Default channel that the sendshit 'command' sends to.
+MESSAGE_CHANNEL: int = 123  # Default channel that counts messages.
 PREFIX: str = '+'  # Bot prefix.
 TOKEN: str = 'discord token'  # Change to your bot token.
